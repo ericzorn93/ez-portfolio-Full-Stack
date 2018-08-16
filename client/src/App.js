91998d505a6303948a21701cff81e78137dea374
Eric Zorn <ericzorndesigns@gmail.com>
2018-08-16 13:52:31 -0400

    Setting React proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/api/orders").then(data => this.setState({ posts: data[0] }));
+  }
+
   render() {
+    const { posts } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{posts}</p>
       </div>
     );
   }
