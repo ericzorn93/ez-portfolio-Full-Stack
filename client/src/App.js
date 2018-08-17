4b7fc9e1cb922b2aded6eae4a3777eae6c7a4d55
Eric Zorn <ericzorndesigns@gmail.com>
2018-08-17 11:04:02 -0600

    Added React Bootstrap, Items, and Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from './components/Navbar';
+import Items from './components/Items';
 
 class App extends Component {
-  state = {
-    posts: [],
-  }
   
   render() {
-    const { posts } = this.state;
-    console.log(posts);
-
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Navbar/>
+        <Items/>
       </div>
     );
   }
---
+++ client/src/components/Items.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
+
+export default class Items extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Items Works</h1>
+      </div>
+    )
+  }
+}
