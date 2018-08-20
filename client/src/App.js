1c50e26f78ce0a71ad6e4001f7af8cf7c4d94147
Eric Zorn <ericzorndesigns@gmail.com>
2018-08-20 13:09:54 -0400

    Adding Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import NavHead from './components/Navbar';
 import Items from './components/Items';
+import Footer from './components/Footer';
 
 class App extends Component {
   
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <NavHead/>
         <Items/>
+        <Footer/>
       </div>
     );
   }
