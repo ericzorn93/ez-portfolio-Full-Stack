fe017a173329f83e02026d0c8fbfae51e547101e
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-08-30 11:47:08 -0400

    Home Component Creation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 import "./App.css";
 import NavHead from './components/Navbar';
+import Home from './components/main-components/Home';
 import Games from './components/main-components/Games';
 import About from './components/main-components/About';
 import Footer from './components/main-components/Footer';
@@ -14,6 +15,7 @@ class App extends Component {
         <div className="App">
           <NavHead/>
           <br/>
+          <Route exact component={Home} path="/"></Route>
           <Route exact component={Games} path="/games"></Route>
           <Route exact component={About} path="/about"></Route>
           <Footer/>
---
+++ client/src/components/main-components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="home-header">Welcome to Daniel Kitchen Gaming - <strong>Home</strong></h1>
+      </div>
+    )
+  }
+}
