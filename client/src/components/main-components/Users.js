aad35fdffa4fbf283d0f6735f39562c62697c797
Eric Zorn <ericzorn@Erics-MBP-2018.lan>
2018-09-09 13:02:35 -0400

    Basic User and Users Front-End Formatting

--- client/src/components/main-components/Users.js
+++ client/src/components/main-components/Users.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import Navigation from '../sub-components/Navigation';
+import User from '../sub-components/User';
+import '../../assets/css/admin/users.css';
+import axios from 'axios';
 
 class Users extends Component {
+
+    state = {
+        users: [
+            { id: 1, name: "Eric Zorn", email: "test", phone: "2016252453", message: "This is a test Message", date: "2018-09-09T16:21:51.231Z" },
+            { id: 2, name: "Keith Zorn", email: "second", phone: "2222222222", message: "This is a another Message", date: "2017-09-09T16:21:51.231Z" }
+        ]
+    };
+
+    componentDidMount = () => {
+
+    };
+
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <Navigation/>
-                <h1>Users</h1>
-            </div>
+                <div className="container">
+                    <h1 className={"user-title"}>All Users</h1>
+                    <User users={this.state.users}/>
+                </div>
+            </React.Fragment>
         );
     }
 }
--- client/src/components/sub-components/User.js
+++ client/src/components/sub-components/User.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Moment from 'react-moment';
+import '../../assets/css/admin/user.css';
 
-const User = (props) => {
+const User = ({ users }) => {
   return (
-    <div>
-      
-    </div>
+     <div className={"user-container"}>
+         {users && users.map(user => {
+           const newPhone = `(${user.phone.slice(0,3)})-${user.phone.slice(3,6)}-${user.phone.slice(6)}`;
+           return(
+               <div className="user container" key={user.id}>
+                 <h4><strong>Name:</strong> {user.name}</h4>
+                 <h4><strong>Email:</strong> {user.email}</h4>
+                 <h4><strong>Phone:</strong> {newPhone}</h4>
+                 <h4><strong>Message:</strong> {user.message}</h4>
+                 <h4>
+                     <strong>Created At:</strong> <Moment to={user.date}></Moment>
+                 </h4>
+               </div>
+           )
+         })}
+     </div>
   )
-}
+};
 
 export default User;
