7720d6208d102b8ba15f90bee0e113444152076e
Eric Zorn <ericzorn@Erics-MBP-2018.lan>
2018-09-09 12:25:25 -0400

    Begin Users Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/main-components/Home';
 import About from './components/main-components/About';
 import Contact from "./components/main-components/Contact";
+import Users from "./components/main-components/Users";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path={'/'} component={Home} />
             <Route exact path={'/about'} component={About} />
             <Route exact path={'/contact'} component={Contact} />
+            <Route exact path={'/admin/users'} component={Users} />
         </Switch>
       </React.Fragment>
     );
---
+++ client/src/components/sub-components/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default User;
