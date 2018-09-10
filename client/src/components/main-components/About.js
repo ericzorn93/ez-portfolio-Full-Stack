0c7b29987a9e7a9bc03c0e267b7c7309396fa3b9
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-09-10 17:15:29 -0400

    Reorganizing About Me

--- client/src/components/main-components/About.js
+++ client/src/components/main-components/About.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from '../sub-components/Navigation';
 import '../../assets/css/about.css';
 import { ProgressBar } from 'react-bootstrap';
+import AboutMe from '../sub-components/About_Me';
 
 class About extends Component {
 
@@ -55,6 +56,7 @@ class About extends Component {
         <div className="row">
           <div className="container">
             <h1 className="about-header text-center">About Me / Skills</h1>
+            <AboutMe/>
             <section className="education-section">
               <h3 className="section-header-text">Education <i className="fas fa-graduation-cap" style={{color: "white", fontSize: 40}}></i></h3>
               <div className="schools container">
@@ -82,11 +84,6 @@ class About extends Component {
             <section className="skills">
               <h3 className="section-header-text">Skills <i className="fas fa-check-double" style={{color: "white", fontSize: 40}}></i></h3>
               <div className="progress-bars container">
-                  <label className={"progressbar-label"}>HTML5:</label>
-                  <ProgressBar now={95} label={"HTML 5"} bsStyle={"warning"} />
-
-                  <label className="progressbar-label">CSS3</label>
-                  <ProgressBar now={90} label={"CSS3"} bsStyle={"warning"} />
 
                   <label className="progressbar-label">JavaScript (ES6, ES7, ES8), jQuery, React, NodeJS, Angular, ExpressJS</label>
                   <ProgressBar>
@@ -98,21 +95,27 @@ class About extends Component {
                     <ProgressBar bsStyle={"success"} now={3} key={6} label={"Express JS"} />
                   </ProgressBar>
 
-                  <label className={"progressbar-label"}>Python &amp; Django Framework</label>
-                  <ProgressBar now={60} label={"Python & Django"} bsStyle={"warning"}/>
-
-                  <label className={"progressbar-label"}>Ruby &amp; Rails Framework</label>
-                  <ProgressBar now={65} label={"Ruby & Rails"} bsStyle={"warning"}/>
-
-                  <label className={"progressbar-label"}>PHP</label>
-                  <ProgressBar now={70} label={"PHP"} bsStyle={"warning"}/>
-
                   <label className={"progressbar-label"}>Relational Database Management Systems (RDBMS)</label>
                   <ProgressBar>
                     <ProgressBar bsStyle={"warning"} label={"MySQL"} now={33.33} />
                     <ProgressBar bsStyle={"success"} label={"PostgreSQL"} now={33.33} />
                     <ProgressBar bsStyle={"warning"} label={"SQL Server"} now={33.33} />
                   </ProgressBar>
+
+                  <label className={"progressbar-label"}>HTML5:</label>
+                  <ProgressBar now={95} label={"HTML 5"} bsStyle={"warning"} />
+
+                  <label className="progressbar-label">CSS3</label>
+                  <ProgressBar now={90} label={"CSS3"} bsStyle={"warning"} />
+
+                  <label className={"progressbar-label"}>PHP</label>
+                  <ProgressBar now={70} label={"PHP"} bsStyle={"warning"}/>
+
+                  <label className={"progressbar-label"}>Ruby &amp; Rails Framework</label>
+                  <ProgressBar now={65} label={"Ruby & Rails"} bsStyle={"warning"}/>
+
+                  <label className={"progressbar-label"}>Python &amp; Django Framework</label>
+                  <ProgressBar now={60} label={"Python & Django"} bsStyle={"warning"}/>
 
                   <label className={"progressbar-label"}>NoSQL Databases</label>
                   <ProgressBar now={60} label={"MongoDB"} bsStyle={"warning"}/>
---
+++ client/src/components/sub-components/About_Me.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../assets/css/about_me.css';
+import ProfilePicture from '../../assets/img/profile-picture-second.jpg';
+
+const AboutMe = (props) => {
+  return (
+    <div className={"about-me-container container"}>
+        <div className="row text-center">
+            <img className={"profile-img"} src={ProfilePicture} alt="Profile Picture"/>
+        </div>
+        <h4 className={"about-me-text"}><a style={{color: "white"}} href="https://github.com/ericzorn93" target={"_blank"}><strong>My Github: https://github.com/ericzorn93</strong></a></h4>
+        <br/>
+        <p className={"about-me-text"}>- Highly motivated, creative, multi-disciplinary Full-Stack Web Developer with a passion for emerging technology, engaging in interactive digital Web Design &amp; Development seeking a full-time opportunity with a progressive technology company as a Full-Stack or Front-End Web Developer.</p>
+
+        <p className={"about-me-text"}>- Possess unique talent to visualize concepts with an understanding of design principles and web protocols such as HTML/CSS, JavaScript/jQuery, React JS, Angular JS, PHP, and Python (Django), and Ruby on Rails with an understanding of web design and browser usability and cross-platform compatibility.</p>
+
+        <p className={"about-me-text"}>- Collaborative team member with a committed personality to deliver results. Adept at learning
+        technology with the recognition that technological change is constant.</p>
+    </div>
+  )
+}
+
+export default AboutMe;
