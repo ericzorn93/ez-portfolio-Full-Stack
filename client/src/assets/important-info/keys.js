b2ab72c2cf5e592f3cb47044238555c53d1d9407
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-09-14 11:57:16 -0400

    Added Updated Map

--- client/src/assets/important-info/keys.js
+++ client/src/assets/important-info/keys.js
@@ -1,5 +1,6 @@
 const keys = {
-    googleMapKey: "AIzaSyDVeilTU07walTiggI-pXhNNMyEDGWJP8g"
+    // googleMapKey: "AIzaSyDVeilTU07walTiggI-pXhNNMyEDGWJP8g",
+    googleMapKey: "AIzaSyAvbaCuupaLFnqe1vWcxtAh_0oOSb3ldbQ"
 };
 
 export default keys;
